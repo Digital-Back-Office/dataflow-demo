070016e62503926b491775d0d163b173374d4936
Harigovind T P <97115844+HariTP@users.noreply.github.com>
2026-06-02 17:34:22 +0530

    changes made

--- Demo 1 - Flight Delay Analysis/airflow/dags/helpers/transform.py
+++ Demo 1 - Flight Delay Analysis/airflow/dags/helpers/transform.py
@@ -1,6 +1,5 @@
-import pandas as pd
-
 def get_delay_distribution(df):
+    import pandas as pd
     bins = list(range(-60, 301, 10))
     df['delay_bin'] = pd.cut(df['DEPARTURE_DELAY'], bins=bins, right=False)
     hist = df['delay_bin'].value_counts().sort_index()
@@ -22,6 +21,7 @@ def get_flight_statistics_summary(df):
     return "flight_statistics_summary", rows
 
 def get_delay_vs_hour(df):
+    import pandas as pd
     df['hour'] = pd.to_datetime(df['SCHEDULED_DEPARTURE']).dt.hour
     hourly_df = df.groupby(['hour', 'AIRLINE']).agg(
         avg_departure_delay=('DEPARTURE_DELAY', 'mean')
@@ -29,6 +29,7 @@ def get_delay_vs_hour(df):
     return "delay_vs_hour", hourly_df.to_dict(orient='records')
 
 def get_hourly_avg_delay(df):
+    import pandas as pd
     df['hour'] = pd.to_datetime(df['SCHEDULED_DEPARTURE']).dt.hour
     agg = df.groupby('hour')['DEPARTURE_DELAY'].mean().reset_index(name='avg_departure_delay')
     return "hourly_avg_delay", agg.to_dict(orient='records')
@@ -40,6 +41,7 @@ def get_top10_origin_airports_by_delay(df):
     return "top10_origin_airports_by_delay", agg.to_dict(orient='records')
 
 def get_weekday_avg_delay(df):
+    import pandas as pd
     df['day_of_week'] = pd.to_datetime(df['SCHEDULED_DEPARTURE']).dt.day_name()
     agg = df.groupby('day_of_week')[['DEPARTURE_DELAY', 'ARRIVAL_DELAY']].mean().reset_index()
     agg.columns = ['day_of_week', 'avg_departure_delay', 'avg_arrival_delay']
